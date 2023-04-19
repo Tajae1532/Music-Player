8d7f59e39269656ea5471297532867451b4e9776
Jayleen Espinal <jayleen.espinal@gmail.com>
2023-04-19 19:12:04 -0400

    Playlist class
    Created the playlist class for the user to be able to add and remove songs

--- Playlist.java
+++ Playlist.java
@@ -3,14 +3,21 @@ import java.util.ArrayList;
 public class Playlist {
     private ArrayList<Song> songs;
 
+    public ArrayList<Song> getSongs()   {
+        return songs;
+    }
+    
+
     public Playlist()   {
         songs = new ArrayList<>();
     }
 
+    //add songs to the playlist
     public void addSong(Song song)  {
         songs.add(song);
     }
 
+    //remove songs to the playlist
     public void removeSong(int index)   {
         if (index >=0 && index < songs.size())  {
             songs.remove(index);
